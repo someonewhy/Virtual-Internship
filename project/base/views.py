d1fcc22e6020cca5cb28ddfa8a340019d2072af5
someonewhy <120322139+someonewhy@users.noreply.github.com>
2023-08-03 23:16:18 +0800

    Исправление файла views для обработки результата

--- project/base/views.py
+++ project/base/views.py
@@ -1,7 +1,6 @@
-# views.py
-
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.exceptions import APIException
 from .models import PerevalAdd, PerevalImages, User
 from .serializers import (
     PerevalAddSerializer,
@@ -16,15 +15,27 @@ class PerevalAddViewSet(viewsets.ModelViewSet):
     serializer_class = PerevalAddSerializer
 
     def create(self, request, *args, **kwargs):
-        # Используйте PerevalAddCreateSerializer при создании новой записи
         serializer = PerevalAddCreateSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        instance = serializer.save()
 
-        # Вы можете выполнить дополнительные действия здесь, если необходимо
-        # ...
+        try:
+            instance = serializer.save()
+        except Exception as e:
+            raise APIException(detail="Ошибка при выполнении операции", code=500)
 
-        return Response(PerevalAddSerializer(instance).data)
+        response_data = {
+            'status': 200,  # Код HTTP успешного ответа
+            'message': 'Отправлено успешно',
+            'id': instance.pk,  # id вставленной записи
+        }
+
+        return Response(response_data, status=200)  # Ответ с данными о статусе и id
+
+    def handle_exception(self, exc):
+        if isinstance(exc, APIException):
+            return Response({'status': exc.status_code, 'message': exc.detail}, status=exc.status_code)
+
+        return super().handle_exception(exc)
 
 
 class PerevalImagesViewSet(viewsets.ModelViewSet):
